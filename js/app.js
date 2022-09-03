a28c872165715754814df576fde00bddf6625ba2
khorshed-alam-kbd <khorshedalam.kbd@gamil.com>
2022-09-04 05:04:54 +0600

    items found added

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ const loadNewsCategories = () => {
 const displayNewsCategories = categories => {
 
     const newsCategoriesContainer = document.getElementById('news-categories-container');
+
     categories.forEach(category => {
         // console.log(category.category_id);
         const categoryList = document.createElement('li');
@@ -16,13 +17,14 @@ const displayNewsCategories = categories => {
         <a onclick="loadNewsCategoriesDetail('${category.category_id}')" class="nav-link" aria-current="page" href="#">${category.category_name}</a>
         `
         newsCategoriesContainer.appendChild(categoryList);
+
     });
+
 
 }
 const loadNewsCategoriesDetail = (idNewsCategories) => {
     toggleSpinner(true);
     const url = ` https://openapi.programming-hero.com/api/news/category/${idNewsCategories}`;
-    // console.log(url);
     fetch(url)
         .then(response => response.json())
         .then(detail => displayLoadNewsCategoriesDetail(detail.data));
@@ -31,10 +33,14 @@ const loadNewsCategoriesDetail = (idNewsCategories) => {
 
 const displayLoadNewsCategoriesDetail = newsCategoriesDetail => {
     console.log(newsCategoriesDetail);
+    const itemsFounded = document.getElementById('input-flied');
+    itemsFounded.value = `${newsCategoriesDetail.length}  items found for this category`;
     const detailContainer = document.getElementById('detail-container');
     detailContainer.innerHTML = '';
 
     newsCategoriesDetail.forEach(newsDetails => {
+        // console.log(newsDetails.length);
+
         const newsCategoriesDetailDiv = document.createElement('div');
         newsCategoriesDetailDiv.classList.add('col');
         newsCategoriesDetailDiv.innerHTML = `
@@ -45,15 +51,18 @@ const displayLoadNewsCategoriesDetail = newsCategoriesDetail => {
                             </div>
                             <div class="col-md-9">
                                 <div class="card-body mt-4 me-4 mb-4">
-                                    <h5 class="card-title">${newsDetails.title}</h5>
-                                    <p class="card-text text-truncate">${newsDetails.details} </p>
+                                    <h5 class="card-title fw-bold">${newsDetails.title}</h5>
+                                    <p class="card-text">${newsDetails.details.slice(0, 650)}...</p>
                                     <div class= "d-flex justify-content-between align-items-center"> 
                                         <div class="d-flex align-items-center">
                                             <img src="${newsDetails.author.img}" class="author-img">
-                                            <p>${newsDetails.author.name}<br>${newsDetails.author.published_date}</p>
+                                            <div>
+                                                <p class="fw-bold">${newsDetails.author.name} </p>
+                                                <p> ${newsDetails.author.published_date}</p>
+                                            </div>
                                         </div>
-                                        <div> <p> Total View: ${newsDetails.total_view}</p> </div>
-                                        <div> <p> Rating: ${newsDetails.rating.number}</p> </div>
+                                        <div> <p class="fw-bold"> Total View: ${newsDetails.total_view}</p> </div>
+                                        <div> <p class="fw-bold"> Rating: ${newsDetails.rating.number}</p> </div>
                                         <div> <i class="fa-solid fa-arrow-right"></i> </div>
                                     </div>
                                     
